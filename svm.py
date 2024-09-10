a61a2f27e110f3fb56364f60dfcd226bef50fac4
kbjuyeon <kbjuyeon@naver.com>
2024-09-10 11:35:03 +0900

    commit

--- svm.py
+++ svm.py
@@ -8,7 +8,8 @@ from sklearn.metrics import mean_squared_error, r2_score
 data = pd.read_csv('Diabetes main.csv')
 
 X = data.drop(['Outcome', 'BMI'], axis=1)
-y = data['BMI']# 데이터 분할
+y = data['BMI']
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 scaler = StandardScaler()
@@ -17,10 +18,11 @@ X_test_scaled = scaler.transform(X_test)
 
 svr_model = SVR(kernel='rbf', C=100, epsilon=0.1)
 svr_model.fit(X_train_scaled, y_train)
+
 y_pred = svr_model.predict(X_test_scaled)
 
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
-print(f"Mean Squared Error: {mse:.4f}")
-print(f"R-squared Score: {r2:.4f}")
+print(f"평균 제곱 오차: {mse:.4f}")
+print(f"R-제곱 점수: {r2:.4f}")
