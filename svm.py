46884044510d764ce67ed40b135788676d4531b1
kbjuyeon <kbjuyeon@naver.com>
2024-09-10 11:20:11 +0900

    first commit

---
+++ svm.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
+from sklearn.metrics import mean_squared_error, r2_score
+
+data = pd.read_csv('Diabetes main.csv')
+
+X = data.drop(['Outcome', 'BMI'], axis=1)
+y = data['BMI']# 데이터 분할
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+svr_model = SVR(kernel='rbf', C=100, epsilon=0.1)
+svr_model.fit(X_train_scaled, y_train)
+y_pred = svr_model.predict(X_test_scaled)
+
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f"Mean Squared Error: {mse:.4f}")
+print(f"R-squared Score: {r2:.4f}")
